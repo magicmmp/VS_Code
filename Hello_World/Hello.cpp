8ae5a974b8f8743ce5bb3ad35b4f915f13ae7304
magicmmp <2233973404@qq.com>
2019-01-25 03:38:17 +0800

    add file :Hello.cpp

---
+++ Hello_World/Hello.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<unistd.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello VS Code!"<<endl;
+    vector<int> V;
+    for(int i=10;i>0;i--)
+        v.push_back(i);
+    cout<<"The elements in vector:"<<endl;
+    for(auto e :v)
+        cout<<" "<<e;
+    cout<<endl;
+
+
+    return 0;
+}
