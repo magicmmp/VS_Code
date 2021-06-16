ddddae3bda055bf27c022c68b58c55ec494eceed
magicmmp <2233973404@qq.com>
2021-06-16 11:20:57 +0800

    git push with shell: 2021年06月16日 11:20:56

---
+++ Java_hello/src/App.java
@@ -0,0 +1,10 @@
+import com.google.gson.Gson;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Gson gson=new Gson();
+        
+        System.out.println("Hello, World!"+gson);
+        
+    }
+}
---
+++ java_maven_project_test/src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson=new Gson();
+        System.out.println( "Hello World!"+gson );
+    }
+}
